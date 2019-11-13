8db10a8b2acb009a464e50ddc8c23ee1e8f55a1e
hardyeustace <sarahkin@gmail.com>
2019-11-13 21:46:24 +0000

    Update solution_0a938d79.py
    working code - failing for 2 out of   4 tests

--- src/solution_0a938d79.py
+++ src/solution_0a938d79.py
@@ -9,48 +9,39 @@ import json
 import numpy as np
 
 
+
 def read_json_file(fileName):
     with open(fileName, 'r') as f:
         return json.load(f)   
     
-def will_be_vertical_line(ip_array, rows):
-#     if (ip[0] == 0 or ip2[0])
-    print(ip_array[7][5])
-#     print(ip_array.shape)
-#     print(ip)
-    
-def find_details(df_io):
+def will_be_vertical_line(no_rows, rows):
     """
-    >>> find(pd.DataFrame({'input': [[0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], 'output': [[0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0], [0, 0, 0, 0, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0, 2, 0, 8, 0]]}))
-    1
-    """ 
+    >>> will_be_vertical_line(10, [0, 9])
+    True
+    >>> will_be_vertical_line(10, [2, 9])
+    False
+    """
+    return no_rows - 1 in rows and 0 in rows
+    
+def solve(df_io):
     ip = np.array(df_io['input'])
     (rows, cols) = (np.nonzero(ip))
-    will_be_vertical_line(ip, ip1, ip2)
-    print(df_io['input'])
-    print(ip)
-    print(ip.shape)
-    print(ip1, ip2)
-    return 1
-
-def solve(df_io):
-#     """
-#     >>> calc_total_error( pd.DataFrame({'Name':[12, 14, 16, 12, 17, 11], 'Age':[20, 21, 19, 18, 21, 22], \
-#         'variety':['A1', 'B1', 'C1', 'A1', 'A1', 'C1']}), 'variety', ('Age', (21.5, (0.4666666666666666, 'A1', 'C1'))))
-#     0.3333333333333333
-#     """    
-    find_details(df_io)
+    is_vertical = will_be_vertical_line(ip.shape[0], rows)
+    colours = (ip[rows[0]][cols[0]], ip[rows[1]][cols[1]])    
+    if (is_vertical):
+        ip[:, cols[0]::4] = colours[0]
+        ip[:, cols[1]::4] = colours[1]
+    else:
+        ip[rows[0]::4] = colours[0]
+        ip[rows[1]::4] = colours[1]
+    
+    return ip
 
 def main():
-#     content = json.loads('C:/dev/git/ARC/data/training/0a938d79.json')
-#     print(content)
-#     df = pd.read_json('C:/dev/git/ARC/data/training/0a938d79.json')
-#     print(df.head())
     df = read_json_file('C:/dev/git/ARC/data/training/0a938d79.json')
-#     for df_io in df['train']:
-#         print(df_io)
-    solve(df['train'][0])
-    
+#     for df in df['train']:
+#         print(np.array_equal(solve(df), df['output']))
+    print(np.array_equal(solve(df['train'][1]), df['train'][1]['output']))
     
     
 #     for input_output in df['train']:
