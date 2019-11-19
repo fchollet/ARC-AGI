4e5cc73b66dd1787d7a095faa4b5a8f46d0014a9
Ian Matthews <ian.matthews@hpe.com>
2019-11-19 22:39:31 +0000

    Adding initial program structure, .gitignore and updated readme. closes #1

---
+++ src/solution_ed36ccf7.py
@@ -0,0 +1,31 @@
+"""NUI Galway CT5132/CT5148 Programming and Tools for AI (James McDermott)
+
+Solution for Assignment 3: File ed36ccf7.json
+
+Student name(s): Ian Matthews
+Student ID(s):   12100610
+
+"""
+
+
+def solve(input_grid):
+    """
+    Given the input grid from any training or evaluation pair in the input json file,
+    solve should return the correct output grid in the same format.
+    Allowed formats are : 1. a JSON string containing a list of lists; or 2. a Python list of lists;
+    or 3. a Numpy 2D array of type int
+    :param input_grid:
+    :return: output_grid
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    main()
