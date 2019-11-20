7cef276e9bb7c2a000d3f81eea50e1ab6624db6c
Ian Matthews <ian.matthews@hpe.com>
2019-11-20 21:10:56 +0000

    Change input file to c1d99e64.json. closes #5

---
+++ src/solution_c1d99e64.py
@@ -0,0 +1,33 @@
+"""NUI Galway CT5132/CT5148 Programming and Tools for AI (James McDermott)
+
+Solution for Assignment 3: File c1d99e64.json
+
+Student name(s): Ian Matthews
+Student ID(s):   12100610
+
+"""
+import numpy as np
+
+
+def solve(input_grid):
+    """
+    Given the input grid from any training or evaluation pair in the input json file,
+    solve should return the correct output grid in the same format.
+    Allowed formats are : 1. a JSON string containing a list of lists; or 2. a Python list of lists;
+    or 3. a Numpy 2D array of type int
+    :param input_grid:
+    :return: output_grid
+
+    """
+    return input_grid
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    main()
