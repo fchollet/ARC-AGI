e0b1e871ba8fda413420d940364092c4f1f706d1
Jey <jeyavigneshr1394@gmail.com>
2019-11-24 19:15:50 +0000

    Added plotting fuctionality in Read_json

--- src/utils/Read_json.py
+++ src/utils/Read_json.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 
 def json_arc_reader(json_file):
     
@@ -9,3 +10,8 @@ def json_arc_reader(json_file):
     test_inputs = [data['test'][i]['input'] for i in range(len(data['test']))]
     test_outputs = [data['test'][i]['output'] for i in range(len(data['test']))]
     return train_inputs,train_outputs,test_inputs,test_outputs
+
+def visualize(input):
+    for i in range(len(input)):
+        plt.matshow(input[i])
+        plt.show()
