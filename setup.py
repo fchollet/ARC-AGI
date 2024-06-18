76f3d29aabe402b5203112a4fc595723b1fdf2a1
nilsec <nils.eckstein@kaiko.ai>
2024-06-18 14:48:39 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="arc",
+    version="0.1.0",
+    description="arc-hack",
+    url="this",
+    author="NLP",
+    author_email="NLP",
+    packages=["nlp_hack"],
+)
